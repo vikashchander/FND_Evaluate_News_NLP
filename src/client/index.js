988eb61d45ae136cacb5bbd22d0b55dc0b3e76b3
vikash chander <vikashchander78384@gmail.com>
2020-05-10 13:26:35 +0530

    work on set up service workers

--- src/client/index.js
+++ src/client/index.js
@@ -20,3 +20,13 @@ form.addEventListener("submit", e => {
     handleSubmitBtn(url);
   }
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 module.exports = {
@@ -41,5 +42,12 @@ module.exports = {
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
     }),
+    new WorkboxPlugin.GenerateSW({
+             // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+        }),
+  
   ],
 };
